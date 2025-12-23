807e12f364858678309b2c01393433b0896e2d1c
Rohit Kumar <rohitdhankhar7347@gmail.com>
2025-12-23 16:42:43 +0530

    added routers to create api

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 const express = require('express');
 //const {City} = require('./models/index');
 const bodyParser = require('body-parser');
-const CityRepository = require('./repository/city-repository');
 const {PORT }=  require('./config/serviceConfig');
-const city = require('./models/city');
+
+const ApiRoutes = require('./routes/index');
 const setupAndStartServer = async()=>{
     const app = express();
 
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended:true})); 
- 
+    app.use('/api',ApiRoutes);
+    // it will for all routes starting from /api the go in APIRoutes where based on url the routes are divided into v1 and v2
+    // now for api/v1 it will got to routes/v1/index.js and all the routes of v1 are defined here
     app.listen(PORT,()=>{
         console.log(`Server started at ${PORT}`);
-        const repo = new CityRepository();
-       repo.createCity({name:"Jaipur"});
-        //repo.deleteCity(4);
+       
         
     })
 }
