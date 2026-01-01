bc4d7c2dcd1e1e67d8e79c212e97152c1b9f05c3
Rohit Kumar <rohitdhankhar7347@gmail.com>
2026-01-01 13:38:10 +0530

    added seeders and airplanes model

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const {Airport,City} = require('./models/index');
 const ApiRoutes = require('./routes/index');
 const setupAndStartServer = async()=>{
     const app = express();
-
+    app.use(express.json());
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended:true})); 
     app.use('/api',ApiRoutes);
